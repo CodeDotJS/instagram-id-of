90bf0971b30d8e49721d8b605ee5740432f86c58
CodeDotJS <rushforlinux@gmail.com>
2016-02-03 04:30:12 +0530

    trimmed whitespaces

--- cli.js
+++ cli.js
@@ -3,15 +3,15 @@ const https = require('follow-redirects').https;
 const colors = require('colors');
 
 colors.setTheme({
-     error: ['red', 'bold']
+	error: ['red', 'bold']
 });
 
 colors.setTheme({
-    info: ['cyan', 'bold']
+	info: ['cyan', 'bold']
 });
 
 colors.setTheme({
-    normal: ['green', 'bold']
+	normal: ['green', 'bold']
 });
 
 const argv = require('yargs')
@@ -20,31 +20,30 @@ const argv = require('yargs')
     .argv;
 
 const options = {
-    hostname: 'www.instagram.com',
-    port: 443,
-    path: argv.u,
-    method: 'GET',
-    headers: {
-        'accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-        'Host': 'www.instagram.com',
-        'Connection': 'Keep-Alive',
-        'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6'
-    }
+	hostname: 'www.instagram.com',
+	port: 443,
+	path: argv.u,
+	method: 'GET',
+	headers: {
+		'accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
+		'Host': 'www.instagram.com',
+		'Connection': 'Keep-Alive',
+		'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6'
+	}
 };
 
 const req = https.request(options, function (res) {
-    if (res.statusCode === 200) {
-        console.log("\nStatus Code: ".info, "😀".info); // res.statusCode
-    } else {
-        console.log("\nStatus Code: ".error, "😥".info);
-    }
-    var store = "";
-    res.setEncoding('utf8');
-    res.on('data', function (d) {
-        store += d;
-
-    });
+	if (res.statusCode === 200) {
+		console.log('\nStatus Code: '.info, '😀'.info); // res.statusCode
+	} else {
+		console.log("\nStatus Code: ".error, "😥".info);
+	}
+		var store = "";
+		res.setEncoding('utf8');
+		res.on('data', function (d) {
+			store += d;
+		});
     res.on('end', function () {
         const rePattern = new RegExp(/id":"\d*/);
         const arrMatches = store.match(rePattern);
