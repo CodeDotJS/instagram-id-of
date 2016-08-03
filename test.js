791b5e048a53cfe72befb8ac0edea34d10be73e7
CodeDotJS <rushforlinux@gmail.com>
2016-08-03 07:14:00 +0530

    passed

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ import childProcess from 'child_process';
 import test from 'ava';
 
 test.cb(t => {
-	childProcess.execFile('./cli.js', ['-u'], {
+	childProcess.execFile('./cli.js', ['iama_rishi'], {
 		cwd: __dirname
 	}, (err, stdout) => {
 		t.ifError(err);
-		t.true(stdout === '\n ❱ Instagram User  :  ✔\n\n ❱ User ID         :  345375353 \n\n');
+		t.true(stdout === '\u001b[?25l\n› Fetching User ID. Please wait!\n\u001b[?25l\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\n› iama_rishi is an Instagram user!\n\n\u001b[?25l\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\n› Instagram ID of iama_rishi is 3162844793\n\n\u001b[?25h');
 		t.end();
 	});
 });
