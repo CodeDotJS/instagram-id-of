8c384a7bd226c1aba6db70daae2ca5f664313a9a
Rishi Giri <rushforlinux@gmail.com>
2018-03-28 01:54:35 +0530

    update tests

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 import childProcess from 'child_process';
-
 import test from 'ava';
 
-test.cb(t => {
-	childProcess.execFile('./cli.js', ['iama_rishi'], {
-		cwd: __dirname
-	}, (err, stdout) => {
-		t.ifError(err);
-		t.true(stdout === `\u001b[?25l\n\u001b[?25l\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\n› User ID of 10x RISHI is 3162844793 \n\n\u001b[?25h`);
+test.cb('default', t => {
+	const cp = childProcess.spawn('./cli.js', {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 1);
 		t.end();
 	});
 });
