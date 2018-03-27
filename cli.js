a890c25d1b68bd56729c3c61f288a11c73e2fdb9
Rishi Giri <rushforlinux@gmail.com>
2018-03-27 21:58:32 +0530

    update codebase

--- cli.js
+++ cli.js
@@ -3,70 +3,51 @@
 'use strict';
 
 const dns = require('dns');
-const https = require('follow-redirects').https;
+const got = require('got');
+const chalk = require('chalk');
 const logUpdate = require('log-update');
-const colors = require('colors/safe');
 const ora = require('ora');
 const updateNotifier = require('update-notifier');
 const pkg = require('./package.json');
 
 updateNotifier({pkg}).notify();
+
 const arg = process.argv[2];
+const end = process.exit;
 const spinner = ora();
+
+const profile = `https://instagram.com/${arg}/?__a=1`;
 
 if (!arg || arg === '-h' || arg === '--help') {
 	console.log(`
- ${colors.cyan('Usage   :')} insta-id-of ${colors.blue('<username>\n')}
- ${colors.cyan('Example :')} insta-id-of ${colors.yellow('iama_rishi\n')}
- ${colors.cyan('Help    :')} insta-id-of ${colors.green('-h')} ${colors.dim('--help')}
- `);
-	process.exit(1);
+  ${chalk.cyan('Usage')}   :  insta-id-of <username>
+
+  ${chalk.cyan('Example')} :  insta-id-of 9gag
+
+  ${chalk.cyan('Help')}    :  insta-id-of ${chalk.dim('-h or --help')}
+	`);
+	end(1);
 }
 
-const options = {
-	hostname: 'www.instagram.com',
-	port: 443,
-	path: `/${arg}`,
-	method: 'GET',
-	headers: {
-		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-		'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6'
-	}
-};
-
 dns.lookup('instagram.com', err => {
-	if (err && err.code === 'ENOTFOUND') {
-		logUpdate(`\n${colors.red.bold('›')} Please check your internet connection`);
-		process.exit(1);
+	if (err) {
+		logUpdate(`\n ${chalk.red('›')} ${chalk.dim('Please check your internet connection!')} \n`);
+		end(1);
 	} else {
 		logUpdate();
-		spinner.text = `Fetching user id. Please wait`;
+		spinner.text = chalk.blue('Fetching...');
 		spinner.start();
 	}
 });
 
-const req = https.request(options, res => {
-	if (res.statusCode === 200) {
-		spinner.text = `${arg} is an Instagram user!`;
-	} else {
-		logUpdate(`${colors.cyan.bold('\n›')} ${colors.dim(`Sorry "${arg}" is not an Instagram user!`)}\n`);
-		process.exit(1);
+got(profile, {json: true}).then(res => {
+	const userId = res.body.graphql.user.id;
+	const userName = res.body.graphql.user.full_name || arg;
+	logUpdate(`\n${chalk.cyan('›')} User ID of ${chalk.cyan(userName)} is ${chalk.yellow(userId)} \n`);
+	spinner.stop();
+}).catch(err => {
+	if (err) {
+		logUpdate(`\n${chalk.red('›')} ${chalk.yellow(arg)} is not an instagram user! \n`);
+		end(1);
 	}
-	let store = '';
-	res.setEncoding('utf8');
-
-	res.on('data', d => {
-		store += d;
-	});
-
-	res.on('end', () => {
-		const rePattern = new RegExp(/id": "\d*/);
-		const arrMatches = store.match(rePattern);
-
-		if (arrMatches && arrMatches[0]) {
-			logUpdate(`${colors.cyan.bold('\n›')} User id of ${arg} is : ${arrMatches[0].replace('id": "', '')}\n`);
-			spinner.stop();
-		}
-	});
 });
-req.end();
