c5b39198ce87e7a9a5467de5388716d8863839a2
Rishi Giri <rushforlinux@gmail.com>
2018-03-27 21:58:53 +0530

    update output

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ test.cb(t => {
 		cwd: __dirname
 	}, (err, stdout) => {
 		t.ifError(err);
-		t.true(stdout === '\u001b[?25l\n\u001b[?25l\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\n› User id of iama_rishi is : 3162844793\n\n\u001b[?25h');
+		t.true(stdout === `\u001b[?25l\n\u001b[?25l\u001b[1000D\u001b[K\u001b[1A\u001b[1000D\u001b[K\n› User ID of 10x RISHI is 3162844793 \n\n\u001b[?25h`);
 		t.end();
 	});
 });
