30710ee30ef42142f6d69f9c7bfe88aef5fe30f6
Rishi Giri <rushforlinux@gmail.com>
2018-05-02 21:23:16 +0530

    update tests

--- test.js
+++ test.js
@@ -11,3 +11,14 @@ test.cb('default', t => {
 		t.end();
 	});
 });
+
+test.cb('user', t => {
+	const cp = childProcess.spawn('./cli.js', ['iama_rishi'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
